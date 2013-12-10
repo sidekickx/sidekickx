9fd4feb9067570f548e65e0bf051472d1b576b81
sidekickx <emma_sjostedt@hotmail.com>
2013-12-10 14:11:13 +0100

    färdig labb 3 övning1

---
+++ lab3ö1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read -p "skriv ett tal " x
+read -p "skriv ett tal till " y
+
+if [ $x -eq $y ]; then
+    echo "$x är lika med $y"
+else [ $x -ne $y ]; 
+    echo "$x är inte lika med $y"
+fi
+if [ $x -lt $y ]; then 
+    echo "$x är mindre än $y"
+elif [ $x -gt $y ]; then 
+    echo "$x är större än $y"
+else
+    echo ""
+fi
+
