12d0214d65c8262ffeec90c338d459920cc2e45f
sidekickx <emma_sjostedt@hotmail.com>
2013-12-10 11:19:53 +0100

    variation på första

---
+++ lab2ö1.2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/home/emma/bash-grund
+Var=`/bin/pwd`
+Namn=`/usr/bin/whoami`
+    echo "Hej $Namn, du befinner dig i katalogen: $Var."
+
+
+
